40a95486d994ecd6094cb77748d352826ca9fb61
Sinergia-Chile <informatica@sinergia-chile.cl>
2017-10-12 16:55:42 -0300

    sinergia

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+//import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  url: string;
+ // templateurl: 'sinergia.html';
 
+  constructor(private InAppBrowser: InAppBrowser , private loadingCtrl: LoadingController) { 
+
+    this.presentLoading();
+   // this.templateurl; 
+    this.OpenWebpage('https://www.sinergia-chile.cl');
   }
+
+  OpenWebpage(url:string)
+  {
+    const option: InAppBrowserOptions = {
+      zoom: 'no'
+    }
+    const browser = this.InAppBrowser.create(url, '_self',option); 
+    browser.show();   
+  }
+
+  //constructor(public loadingCtrl: LoadingController) { }
+
+  presentLoading() {
+    this.loadingCtrl.create({
+      content: 'Bienvenido a Sinergia-Chile...',
+      duration: 5000,
+      dismissOnPageChange: true
+    }).present();
+   
+   
+    
+  }
+
+  
+
+  
 
 }
